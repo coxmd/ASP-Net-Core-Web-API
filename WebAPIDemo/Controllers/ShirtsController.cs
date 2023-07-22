09fa00a24277077b729266881f332d26154ee73c
coxmd <coxmusyoki@gmail.com>
2023-07-22 11:06:59 +0300

    Converted architecture from minimalistic to MVC

---
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPIDemo.Controllers
+{
+    [ApiController]
+    public class ShirtsController : ControllerBase
+    {
+        public string GetShirts()
+        {
+            return "Reading All Shirts";
+        }
+
+        public string GetShirtById(int id)
+        {
+            return $"Reading shirt: {id}";
+        }
+
+        public string CreateShirt()
+        {
+            return $"Creating a shirt";
+        }
+
+        public string UpdateShirt(int id) 
+        {
+            return $"Updating Shirt: {id}";
+        }
+    }
+}
