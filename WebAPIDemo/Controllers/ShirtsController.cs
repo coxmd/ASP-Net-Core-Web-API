805b66ef3b0c31922c160955296d12da39325aec
coxmd <coxmusyoki@gmail.com>
2023-07-22 11:58:58 +0300

    Mapped http request to each of the action methods

--- WebAPIDemo/Controllers/ShirtsController.cs
+++ WebAPIDemo/Controllers/ShirtsController.cs
@@ -3,26 +3,37 @@
 namespace WebAPIDemo.Controllers
 {
     [ApiController]
+    [Route("api/[controller]")]
     public class ShirtsController : ControllerBase
     {
+        [HttpGet]
         public string GetShirts()
         {
             return "Reading All Shirts";
         }
 
+        [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading shirt: {id}";
         }
 
+        [HttpPost]
         public string CreateShirt()
         {
             return $"Creating a shirt";
         }
 
+        [HttpPut("{id}")]
         public string UpdateShirt(int id) 
         {
             return $"Updating Shirt: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting Shirt: {id}";
         }
     }
 }
